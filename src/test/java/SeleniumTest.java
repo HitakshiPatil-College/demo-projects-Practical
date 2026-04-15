fc06b6cb2bbecc0f435fc4f84e2b76734340ac8e
HitakshiPatil-College <hit.pat.rt23@dypatil.edu>
2026-04-15 22:41:18 +0530

    Cleanup: Removed compilation artifacts and fixed syntax

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,152 +1,32 @@
+package com.hitakshi.devops;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import io.github.bonigarcia.wdm.WebDriverManager; // Add this import
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SeleniumTest {
     @Test
     public void testTomcatHome() {
-        // 1. Fix Permissions: Save driver in project folder instead of System32
         System.setProperty("wdm.cachePath", System.getProperty("user.dir") + "/target/webdriver");
-        
-        // 2. Setup Driver automatically
         WebDriverManager.chromedriver().setup();
 
-        // 3. Configure Headless Mode (Mandatory for Jenkins)
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless"); 
         options.addArguments("--disable-gpu");
         options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--window-size=1920,1080");
 
         WebDriver driver = new ChromeDriver(options);
-        
+
         try {
-            // 4. Run the Test
             driver.get("http://localhost:8081");
             String title = driver.getTitle();
             System.out.println("Page Title is: " + title);
-            
             Assert.assertTrue(title.contains("Tomcat"), "Title should contain Tomcat");
         } finally {
-            // 5. Always close the browser
-            driver.quit();
-        }
-    }
-}import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import io.github.bonigarcia.wdm.WebDriverManager; // Add this import
-import org.testng.Assert;
-import org.testng.annotations.Test;
-
-public class SeleniumTest {
-    @Test
-    public void testTomcatHome() {
-        // 1. Fix Permissions: Save driver in project folder instead of System32
-        System.setProperty("wdm.cachePath", System.getProperty("user.dir") + "/target/webdriver");
-        
-        // 2. Setup Driver automatically
-        WebDriverManager.chromedriver().setup();
-
-        // 3. Configure Headless Mode (Mandatory for Jenkins)
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless"); 
-        options.addArguments("--disable-gpu");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--window-size=1920,1080");
-
-        WebDriver driver = new ChromeDriver(options);
-        
-        try {
-            // 4. Run the Test
-            driver.get("http://localhost:8081");
-            String title = driver.getTitle();
-            System.out.println("Page Title is: " + title);
-            
-            Assert.assertTrue(title.contains("Tomcat"), "Title should contain Tomcat");
-        } finally {
-            // 5. Always close the browser
-            driver.quit();
-        }
-    }
-}import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import io.github.bonigarcia.wdm.WebDriverManager; // Add this import
-import org.testng.Assert;
-import org.testng.annotations.Test;
-
-public class SeleniumTest {
-    @Test
-    public void testTomcatHome() {
-        // 1. Fix Permissions: Save driver in project folder instead of System32
-        System.setProperty("wdm.cachePath", System.getProperty("user.dir") + "/target/webdriver");
-        
-        // 2. Setup Driver automatically
-        WebDriverManager.chromedriver().setup();
-
-        // 3. Configure Headless Mode (Mandatory for Jenkins)
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless"); 
-        options.addArguments("--disable-gpu");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--window-size=1920,1080");
-
-        WebDriver driver = new ChromeDriver(options);
-        
-        try {
-            // 4. Run the Test
-            driver.get("http://localhost:8081");
-            String title = driver.getTitle();
-            System.out.println("Page Title is: " + title);
-            
-            Assert.assertTrue(title.contains("Tomcat"), "Title should contain Tomcat");
-        } finally {
-            // 5. Always close the browser
-            driver.quit();
-        }
-    }
-}import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import io.github.bonigarcia.wdm.WebDriverManager; // Add this import
-import org.testng.Assert;
-import org.testng.annotations.Test;
-
-public class SeleniumTest {
-    @Test
-    public void testTomcatHome() {
-        // 1. Fix Permissions: Save driver in project folder instead of System32
-        System.setProperty("wdm.cachePath", System.getProperty("user.dir") + "/target/webdriver");
-        
-        // 2. Setup Driver automatically
-        WebDriverManager.chromedriver().setup();
-
-        // 3. Configure Headless Mode (Mandatory for Jenkins)
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless"); 
-        options.addArguments("--disable-gpu");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--window-size=1920,1080");
-
-        WebDriver driver = new ChromeDriver(options);
-        
-        try {
-            // 4. Run the Test
-            driver.get("http://localhost:8081");
-            String title = driver.getTitle();
-            System.out.println("Page Title is: " + title);
-            
-            Assert.assertTrue(title.contains("Tomcat"), "Title should contain Tomcat");
-        } finally {
-            // 5. Always close the browser
             driver.quit();
         }
     }
